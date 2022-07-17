b386db42e8ff12a664840c0a5b44c9383d15fc09
unknown <m9500013757@gmail.com>
2022-07-17 17:24:42 +0300

    add 16 task

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputFirst=Console.ReadLine();
+string? inputSecond=Console.ReadLine();
+
+if ((inputFirst!=null)&&(inputSecond!=null))
+{
+    int inputNumberFirst=int.Parse(inputFirst);
+    int inputNumberSecond=int.Parse(inputSecond);
+
+    int a=inputNumberFirst*inputNumberFirst;
+    int b=inputNumberSecond*inputNumberSecond;
+
+    if ((a==inputNumberSecond)||(b==inputNumberFirst))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
