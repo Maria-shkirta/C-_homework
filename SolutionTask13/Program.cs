b6413e79f482f7115cc945d9a736626f0f5fd9fc
unknown <m9500013757@gmail.com>
2022-07-17 17:49:04 +0300

    add 13

---
+++ SolutionTask13/Program.cs
@@ -0,0 +1,12 @@
+﻿string? input=Console.ReadLine();
+
+Char[]output=input.ToCharArray();
+
+if (output.Length>=3)
+{
+    Console.WriteLine(output[2]);
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
