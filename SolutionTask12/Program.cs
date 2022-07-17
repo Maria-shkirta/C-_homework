012f50517115645e46d115344e1b70eed70cf567
unknown <m9500013757@gmail.com>
2022-07-17 14:31:03 +0300

    добавили задачу 12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,20 @@
+﻿//решение Николая
+string? inputNumber1= Console.ReadLine();
+string? inputNumber2= Console.ReadLine();
+
+if (inputNumber1 != null && inputNumber2 != null)
+{
+    int inputNumberOne=int.Parse(inputNumber1);
+    int inputNumberTwo=int.Parse(inputNumber2);
+
+    int div= inputNumberTwo%inputNumberOne;
+
+    if (div==0)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no"+div);
+    }
+}
