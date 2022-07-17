1d9229ffefcc4d86540b9803273d748f2855841e
unknown <m9500013757@gmail.com>
2022-07-17 17:55:01 +0300

    add 15

---
+++ SolutionTask15/Program.cs
@@ -0,0 +1,12 @@
+﻿string? input=Console.ReadLine();
+
+int output=int.Parse(input);
+
+if ((output==6)||(output==7))
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
