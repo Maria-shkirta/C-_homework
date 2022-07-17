f78d6e325349841f5ba4045d9163b1e7259c8e6a
unknown <m9500013757@gmail.com>
2022-07-17 15:03:26 +0300

    add 14 task

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,14 @@
+﻿string? inputNumber=Console.ReadLine();
+    
+            string answer;
+            int num=int.Parse(inputNumber);
+            if (((num%7)==0)&&((num%23)==0))
+            {
+                answer="It's multiple for 7 and 23";
+            }
+            else 
+            {
+                answer="It's not multiple for 7 and 23";
+            }
+            Console.WriteLine(answer);
+    
