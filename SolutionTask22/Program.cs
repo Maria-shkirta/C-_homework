f032dd5a0dadc64aac61e7ab6b471752130f57d4
unknown <m9500013757@gmail.com>
2022-07-20 17:47:16 +0300

    add 22

---
+++ SolutionTask22/Program.cs
@@ -0,0 +1,22 @@
+﻿// на входе число N, на выходе таблица квадратов чисел от 1 до N. 5->1, 4, 9, 16, 25
+
+string? inputLineN=Console.ReadLine();
+
+if (inputLineN!=null)
+{
+    int numberN=int.Parse(inputLineN);
+
+    string LineN="";
+    string LineNN=string.Empty;
+    int s=1;
+
+    while (s<=numberN)
+    {
+        LineN=LineN+s+" ";
+        LineNN=LineNN+(s*s).ToString()+" ";
+        s++;
+
+    }
+    Console.WriteLine(LineN);
+    Console.WriteLine(LineNN);
+}
