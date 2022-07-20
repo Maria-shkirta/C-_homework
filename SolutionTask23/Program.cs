18042560197e59c58d1752dc67bc0fe2389c0c74
unknown <m9500013757@gmail.com>
2022-07-20 17:58:48 +0300

    add 23

---
+++ SolutionTask23/Program.cs
@@ -0,0 +1,22 @@
+﻿// на входе число N, на выходе таблица кубов чисел от 1 до N. 3->1, 8, 27
+
+string? inputLineN=Console.ReadLine();
+
+if (inputLineN!=null)
+{
+    int numberN=int.Parse(inputLineN);
+
+    string LineN="";
+    string LineNN=string.Empty;
+    int s=1;
+
+    while (s<=numberN)
+    {
+        LineN=LineN+s+" ";
+        LineNN=LineNN+(s*s*s).ToString()+" ";
+        s++;
+
+    }
+    Console.WriteLine(LineN);
+    Console.WriteLine(LineNN);
+}
