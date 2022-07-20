532c50b8b2410f7babac9aa9e94ce218b256ce8c
unknown <m9500013757@gmail.com>
2022-07-20 16:08:46 +0300

    add 18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,21 @@
+﻿//по заданному номеру четверти показывает диапозон возможных координат точек в этой области (x и y).
+
+void printAnswer(int number)
+{
+    if (inputNumber==1) Console.WriteLine("Допсутимо: x>0, y>0");
+    if (inputNumber==2) Console.WriteLine("Допсутимо: x<0, y>0");
+    if (inputNumber==3) Console.WriteLine("Допсутимо: x<0, y<0");
+    if (inputNumber==4) Console.WriteLine("Допсутимо: x>0, y<0");
+
+}
+
+string? inputLine=Console.ReadLine();
+
+if (inputLine!=null)
+{
+    int inputNumber=int.Parse(inputLine);
+
+    printAnswer(inputNumber);
+
+
+}
