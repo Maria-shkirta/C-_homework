fe12d13cd50da7dd908245796b75b98631b03f67
unknown <m9500013757@gmail.com>
2022-07-20 16:47:07 +0300

    add 20

---
+++ SolutionTask20/Program.cs
@@ -0,0 +1,38 @@
+﻿//принимает координаты 2 точек и находит расстояние между ними в 2Д. например А(3,6), В(2,1)|=>5,09
+
+int coordXPointA;
+int coordYPointA;
+int coordXPointB;
+int coordYPointB;
+double lengthAB;
+
+
+//считывает координаты A и В
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите координату X точкм А");
+    coordXPointA=int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точкм А");
+    coordYPointA=int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату X точкм B");
+    coordXPointB=int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точкм B");
+    coordYPointB=int.Parse(Console.ReadLine());
+    
+}
+
+
+
+//вычисляет расстояние между А и В
+void calculateLengthAB()
+{
+    lengthAB=Math.Sqrt(Math.Pow((coordXPointA-coordXPointB),2)+Math.Pow((coordYPointA-coordYPointB),2));
+}
+
+readDataOfPoint();
+calculateLengthAB();
+
+Console.WriteLine(lengthAB);
