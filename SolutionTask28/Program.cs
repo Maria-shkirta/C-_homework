849fe65a9cad740a1235ba53155658b5346ab015
unknown <m9500013757@gmail.com>
2022-08-04 23:48:23 +0300

    add SolutionTAsk28

---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,16 @@
+﻿//принимает N, выдает произведение чисел от 1 до N.
+Console.Write("Введите число:");
+string inputLine=Console.ReadLine();
+int inputNum=int.Parse(inputLine);
+
+Console.WriteLine(sumNums(inputNum));
+
+int sumNums(int num)
+{
+    int sum=1;
+    for (int i=1; i<=inputNum; i++)
+    {
+        sum *=i;
+    }
+    return sum;
+}
