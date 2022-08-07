0404b1ed6cab882772834186e52431a3ecd45652
unknown <m9500013757@gmail.com>
2022-08-07 14:09:33 +0300

    add SolutionTAsk25

---
+++ SolutionTask25/Program.cs
@@ -0,0 +1,24 @@
+﻿// принимает два числа А и В, возводит число А в натуральную степень В.
+
+Console.Write("Введите число А: ");
+
+int inputNumberA=int.Parse(Console.ReadLine());
+
+Console.Write("Введите число В: ");
+
+int inputNumberB=int.Parse(Console.ReadLine());
+
+void Calculate()
+{
+    int C=1;
+    int i=0;
+    while (i<inputNumberB)
+    {
+        C=C*inputNumberA;
+        i++;
+    }
+    Console.WriteLine(C);
+
+}
+
+Calculate();
