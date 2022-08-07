6c5e217929c9337a32ec28e77b35b35f9b0a9714
unknown <m9500013757@gmail.com>
2022-08-07 14:35:04 +0300

    add SolutionTAsk29

---
+++ SolutionTask29/Program.cs
@@ -0,0 +1,19 @@
+﻿// задает массив из 8 элементов и выводит их на экран. 6, 33, 1=> [6, 33, 1]
+
+System.Random numberSintezator = new Random();
+
+void Calculate()
+{
+    int i = 0;
+    Console.Write("[");
+    while (i < 8-1)
+    {
+        Console.Write(numberSintezator.Next(0, 100)+",");
+        i++; 
+
+    }
+     Console.Write(numberSintezator.Next(0, 100));
+        Console.Write("]");
+}
+
+Calculate();
