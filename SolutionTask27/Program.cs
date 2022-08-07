c068dcfdb6ee1d93b905dc231d5c5b536e246896
unknown <m9500013757@gmail.com>
2022-08-07 14:26:21 +0300

    add SolutionTAsk27

---
+++ SolutionTask27/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает число, выдает сумму цифр в числе. 82=>10
+
+int inputNumber = int.Parse(Console.ReadLine());
+int B = 0;
+
+void Calculate()
+{
+    while (inputNumber > 0)
+    {
+        B = B + inputNumber % 10;
+        inputNumber = inputNumber / 10;
+    }
+
+}
+
+Calculate();
+
+Console.WriteLine(B);
