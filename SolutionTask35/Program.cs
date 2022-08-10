90655aea0c5211896b83c82653226e2730ed113f
unknown <m9500013757@gmail.com>
2022-08-10 17:37:23 +0300

    add SolutionTAsk35

---
+++ SolutionTask35/Program.cs
@@ -0,0 +1,54 @@
+﻿//задать одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]
+
+//метод заполняет массив
+int[] FillingArray()
+{
+    int[] outArray = new int[123];
+    int i = 0;
+    System.Random numberSintezator = new System.Random();
+    while (i < 123)
+    {
+        outArray[i] = numberSintezator.Next(-1000,1000);
+
+        i++;
+
+
+    }
+    return outArray;
+}
+
+//метод решения задачи 35
+int ColculateTask(int[] inputArray, int downBorder, int upBorder)
+{
+    int resultCount = 0;
+    int i = 0;
+    while (i < inputArray.Length)
+    {
+        if (inputArray[i]>=downBorder&&inputArray[i]<=upBorder)
+        {
+            Console.Write(inputArray[i]+" ");
+            resultCount++;
+        }
+        i++;
+    }
+    return resultCount;
+
+}
+
+//метод печатает массив
+void PrintIntArray(int[] outArray)
+{
+    int i = 0;
+
+    while (i < (outArray.Length - 1))
+    {
+        Console.Write(outArray[i] + ", ");
+        i++;
+
+    }
+}
+
+int[] buferArray=FillingArray();
+PrintIntArray(buferArray);
+Console.WriteLine();
+Console.WriteLine(ColculateTask(buferArray,10,99));
