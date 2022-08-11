e84aefa0fa1d35192a9670b3a1a61b87f4cf353f
unknown <m9500013757@gmail.com>
2022-08-11 18:54:11 +0300

    add SolutionTAsk36

---
+++ SolutionTask36/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечетных позициях. 3,7,23,12- 19
+
+//метод заполняет массив
+int[] FillingArray()
+{
+    int[] outArray = new int[4];
+    int i = 0;
+    System.Random numberSintezator = new System.Random();
+    while (i < 4)
+    {
+        outArray[i] = numberSintezator.Next(1, 10);
+        i++;
+    }
+    return outArray;
+}
+
+
+//метод печатает массив
+void PrintIntArray(int[] outArray)
+{
+    int i = 0;
+
+    while (i < (outArray.Length))
+    {
+        Console.Write(outArray[i] + ", ");
+        i++;
+
+    }
+}
+
+
+//метод решения задачи 34
+int ColculateTask(int[] outArray)
+{
+    int resultNumber = 0;
+
+    int i = 1;
+    while (i < outArray.Length)
+    {
+        resultNumber = resultNumber + outArray[i];
+        i += 2;
+
+    }
+    return resultNumber;
+
+
+}
+
+
+int[] buferArray = FillingArray();
+PrintIntArray(buferArray);
+Console.WriteLine();
+int result = ColculateTask(buferArray);
+Console.Write(result);
