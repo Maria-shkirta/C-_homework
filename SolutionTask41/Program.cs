5bc3554da72559da15268235966ae9e8df8abfce
unknown <m9500013757@gmail.com>
2022-08-13 16:03:21 +0300

    add SolutionTAsk41

---
+++ SolutionTask41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 0, 7, 8, -2, -2 -> 2
+
+int[] Read(int m)
+{
+    int[] newArray=new int[m];
+    int i=0;
+    while (i<m)
+    {
+        newArray[i]=int.Parse(Console.ReadLine());
+    }
+    return newArray;
+}
+
+int Calculate (int[] newArray, int m)
+{
+    int Count=0;
+    int i=0;
+    while (i<m)
+    {
+        if (newArray[i]>0)
+        {
+            Count++;
+        }
+        i++;
+    }
+    return Count;
+}
+
+void PrintResult(int Count)
+{
+    Console.WriteLine(Count);
+}
+
+Console.WriteLine("Введите количество чисел: ");
+int m=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите числа: ");
+int[] num=new int[m];
+Read(m);
+int result = Calculate(num,m);
+PrintResult(result);
