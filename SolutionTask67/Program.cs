e62a0f8e3057a61cb29c249ffc6ae443d0b03de9
unknown <m9500013757@gmail.com>
2022-08-23 21:52:49 +0300

    add SolutionTAsk67

---
+++ SolutionTask67/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int numberM=ReadData("Задайте число M");
+
+Console.WriteLine(Sum(numberM));
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+int Sum(int num)
+{
+    if (num>0)
+    {
+    return num%10+Sum(num/10);
+
+    }
+    else
+    {
+        return 0;
+    }
+}
