37192370478d54ee43705c5931b1b18c09ac54bc
unknown <m9500013757@gmail.com>
2022-08-23 22:08:29 +0300

    add SolutionTAsk64

---
+++ SolutionTask64/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+
+int numberM=ReadData("Задайте число M");
+int numberN=ReadData("Задайте число N");
+
+if (numberM<numberN)
+{
+    NaturalNumberPrinter(numberM,numberN);
+}
+else
+{
+    NaturalNumberPrinter(numberN,numberM);
+}
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+void NaturalNumberPrinter(int m,int n)
+{
+    if (m-1==n)return;
+    Console.WriteLine(m+" ");
+    NaturalNumberPrinter(m+1,n);
+}
+
+
