a6671f2f6f7ce18780b52eb9177eb06a4fb24ac6
unknown <m9500013757@gmail.com>
2022-08-23 21:28:26 +0300

    add SolutionTAsk63

---
+++ SolutionTask63/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте значение N. Напишите программу, которая выведетвсе натуральные числа в промежутке от 1 до N.
+
+int RaedData()
+{
+    Console.WriteLine("Задайте число: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int NuturalNumberPrinter(int num)
+{
+    if (num==2) return 1;
+    else
+    {
+        num--;
+        Console.WriteLine(num);
+        Console.Write(NuturalNumberPrinter(num)+" ");
+
+    }
+    return num;
+}
+
+int inputNumber=RaedData();
+NuturalNumberPrinter(inputNumber+2);
