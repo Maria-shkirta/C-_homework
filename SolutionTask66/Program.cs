8badfb0d3ed7f0eec436165c645fc53f39049d03
unknown <m9500013757@gmail.com>
2022-08-23 22:26:20 +0300

    add SolutionTAsk66

---
+++ SolutionTask66/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+
+int numberM = ReadData("Задайте число M");
+int numberN = ReadData("Задайте число N");
+int sum=0;
+
+if (numberM < numberN)
+{
+    NaturalNumberPrinter(numberM, numberN);
+}
+else
+{
+    NaturalNumberPrinter(numberN, numberM);
+}
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+
+void NaturalNumberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;
+    sum=sum+m;
+    Console.WriteLine(sum);
+    NaturalNumberPrinter(m + 1, n);
+}
