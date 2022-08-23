fb40d77a28d48d9f0e379b302751eb3c19229044
unknown <m9500013757@gmail.com>
2022-08-23 22:01:55 +0300

    add SolutionTAsk69

---
+++ SolutionTask69/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает два числа А и В, и возводит число А в целую степень В с помощью рекурсии.
+
+int numberA=ReadNumber("Задайте число A");
+int numberB=ReadNumber("Задайте число B");
+
+Console.Write(PowNumber(numberA,numberB));
+
+int PowNumber(int a,int b)
+{
+    if (b==1) return a;
+    return a*PowNumber(a,--b);
+}
+
+int ReadNumber(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine()??"");
+}
